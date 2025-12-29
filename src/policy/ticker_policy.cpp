b6081abd9a5cf0869833014e35bfb54d31c8b3b4
Takashi Mori <kanetugu2015@gmail.com>
2025-12-29 12:56:35 +0900

    bug fixed

--- src/policy/ticker_policy.cpp
+++ src/policy/ticker_policy.cpp
@@ -20,15 +20,7 @@ bool TickerPolicy::should_transfer(const std::shared_ptr<ITimeSource>& time_sour
 
 void TickerPolicy::on_transferred(const std::shared_ptr<ITimeSource>& time_source) {
     uint64_t now = time_source->get_microseconds();
-    // This method is called after a transfer has been made.
-    // We need to schedule the next tick.
-    // To prevent drift, we calculate the next tick based on the previous scheduled tick,
-    // not the current time 'now'.
-    // We also loop to ensure the next tick is in the future, in case the system has
-    // lagged for more than one interval.
-    do {
-        next_tick_time_ += interval_;
-    } while (next_tick_time_ <= now);
+    next_tick_time_ = now + interval_;
 }
 
 } // namespace hakoniwa::pdu::bridge
